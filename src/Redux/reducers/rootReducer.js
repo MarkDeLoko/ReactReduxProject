74e7124feda46ad10f090a1f17aa76c4b731b52d
markdeloko <makhtyamov@yandex.ru>
2022-02-11 19:01:23 +0500

    Feature: -Login Page -Axios middleware

--- src/Redux/reducers/rootReducer.js
+++ src/Redux/reducers/rootReducer.js
@@ -3,6 +3,7 @@ import {composeWithDevTools} from '@redux-devtools/extension'
 import authReducer from "./AuthReducer";
 import axiosMiddleware from "../Middlewares/axios";
 import {loginReducer} from "./LoginReducer";
+import thunk from "redux-thunk";
 
 
 const rootReducer = combineReducers({
